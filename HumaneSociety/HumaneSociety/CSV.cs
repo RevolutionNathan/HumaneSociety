7377fa746fb29dce27b55d3c7cad6fe3ea535a9e
Nathan Whitcomb <nathanwhitcomb@MacBook-Pro.local>
2017-05-23 16:45:49 -0500

    initial commit

---
+++ HumaneSociety/HumaneSociety/CSV.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class CSV
+    {
+        public void WriteCSVtoSQL()
+        {
+            var lines = System.IO.File.ReadAllLines(@"/Library/Prefer/Users/nathanwhitcomb/Desktop/devCode/C#/HumaneSociety/HumaneSociety - Sheet1.csvences/SystemConfiguration/");
+            if (lines.Count() == 0) return;
+            var columns = lines[0].Split(',');
+            var table = new MasterListAnimal();
+
+            for (int i = 1; i < lines.Count() - 1; i++)
+                table.Rows.Add(lines[i].Split(','));
+
+            var connection = "Server=localhost;Database=HumaneSociety;Trusted_Connection=True"; ;
+            var sqlBulk = new SqlBulkCopy(connection);
+            sqlBulk.DestinationTableName = "Table1";
+            sqlBulk.WriteToServer(table);
+        }
+    }
+       
+}
---
+++ HumaneSociety/HumaneSociety/SQL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class SQL
+    {
+
+        public string searchName;
+
+        public void InsertIntoDatabase()
+        {
+            using (SqlConnection connect = new SqlConnection())
+            {
+                connect.ConnectionString = "Server=localhost;Database=HumaneSociety;Trusted_Connection=True";
+                connect.Open();
+                string query = "INSERT INTO AnimalsMasterList (Name, RoomNumber, AnimalType) VALUES (@Name, @RoomNumber, @AnimalType)";
+                SqlCommand insertCommand = new SqlCommand(query, connect);
+                insertCommand.Parameters.AddWithValue("@Name", /*humanPlayer.name*/);
+                
+                insertCommand.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
